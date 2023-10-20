ca54c2f0c8ef2083e6e06c883a2da438787c494f
gustavoprehl <gustavo_prehl@hotmail.com>
2023-10-20 16:38:43 -0300

    Adição das funções de consultar vizinhos e verificar se o grafo é conexo

--- Grafo.py
+++ Grafo.py
@@ -7,13 +7,13 @@ def insertEdge(matrix: Matrix):
     destination = int(input("Vértice de destino: "))
     weight = int(
         input("Peso da aresta (ou 1 para aresta não ponderada): "))
-    matrix.add_edge(source, destination, weight)
+    matrix = matrix.add_edge(source, destination, weight)
 
 
 def removeEdge(matrix: Matrix):
     source = int(input("Vértice de origem: "))
     destination = int(input("Vértice de destino: "))
-    matrix.remove_edge(source, destination)
+    matrix = matrix.remove_edge(source, destination)
 
 
 def printGraph(matrix: Matrix, list: List):
@@ -27,13 +27,36 @@ def getVertexDegree(matrix: Matrix, list: List):
     degree = matrix.get_rate(vert)
     print("O grau do vertice ${vert} é ${degree}")
 
+
 def getGraphDegree(matrix: Matrix):
     graphDegree = matrix.get_graphDegree()
     print("Grau do grafo (matriz): ${graphDegree}")
 
 
+def is_connected(matrix: Matrix):
+    if matrix.is_connected():
+        print("O grafo é conexo.")
+    else:
+        print("O grafo não é conexo.")
+
+
+def is_strongly_connected(matrix: Matrix):
+    if matrix.is_strongly_connected():
+        print("O grafo é fortemente conectado.")
+    else:
+        print("O grafo não é fortemente conectado.")
+
+
+def find_neighbors(matrix: Matrix, vert):
+    neighbors = matrix.find_neighbors(vert)
+    if len(neighbors):
+        print("Os vizinhos são: ${neighbors}")
+    else:
+        print("Não há vizinhos")
+
+
 if __name__ == "__main__":
-    digraph = bool(input("O grafo será direcionado? ( 1- Direcionado / 0- Não direcionado)"))
+    digraph = bool(input("O grafo será direcionado? (1- Direcionado / 0- Não direcionado): "))
     n = int(input("Informe o número de vértices: "))
     list = List(n)
     matrix = Matrix(n, digraph)
@@ -43,8 +66,12 @@ if __name__ == "__main__":
         print("1. Inserir aresta")
         print("2. Remover aresta")
         print("3. Mostrar grafo")
-        print("4. Consultar grau do vertice")
-        print("5. Consultar grau do grafo")
+        print("4. Consultar grau do vértice")
+        print("5. Consultar grau do grafo (matriz)")
+        print("6. Consultar vizinhos de um vértice")
+        print("7. Verificar se o grafo é conexo (não direcionado)")
+        if digraph:
+            print("8. Verificar se o grafo é fortemente conectado (direcionado)")
 
         choice = int(input("Escolha uma opção: "))
 
@@ -58,5 +85,12 @@ if __name__ == "__main__":
             getVertexDegree(matrix, list)
         elif choice == 5:
             getGraphDegree(matrix)
+        elif choice == 6:
+            vert = int(input("Qual o vértice que deseja: "))
+            find_neighbors(matrix, vert)
+        elif choice == 7:
+            is_connected(matrix)
+        elif choice == 8 and digraph:
+            is_strongly_connected(matrix)
         else:
             break
--- Matriz.py
+++ Matriz.py
@@ -7,16 +7,15 @@ class Matrix:
     def get_rate(self, vert):
         if vert >= 0 and vert < self.n:
             soma = 0
-            for r in self.n:
+            for r in range(self.n):  # Corrigindo o loop range
                 if self.graph[vert][r] != "-":
                     soma += 1
             return soma
-        return -1
 
     def get_graphDegree(self):
         soma = 0
-        for l in self.n:
-            for c in self.n:
+        for l in range(self.n):  # Corrigindo o loop range
+            for c in range(self.n):  # Corrigindo o loop range
                 if (self.graph[l][c] != '-'):
                     soma += 1
         return soma
@@ -24,10 +23,77 @@ class Matrix:
     def add_edge(self, source, destination, weight=1):
         if source >= 0 and source < self.n and destination >= 0 and destination < self.n:
             self.graph[source][destination] = weight
+        if not self.digraph:
+            self.graph[destination][source] = weight
 
     def remove_edge(self, source, destination):
         if source >= 0 and source < self.n and destination >= 0 and destination < self.n:
-            self.graph[source][destination] = 0
+            self.graph[source][destination] = "-"
+        if not self.digraph:
+            self.graph[destination][source] = "-"
+
+    def find_neighbors(self, vert):    
+        neighbors = []
+
+        for i in range(self.n):
+            if self.graph[vert][i] != "-":
+                neighbors.append(i)
+        return neighbors
+
+    def get_neighbors(self, vert):
+        if vert >= 0 and vert < self.n:
+            neighbors = []
+            for r in range(self.n):
+                if self.graph[vert][r] != '-':
+                    neighbors.append(r)
+            return neighbors
+
+    def is_connected(self):
+        # Esta função verifica se o grafo é conexo (para grafos não direcionados).
+        # Ela utiliza uma busca em profundidade (DFS) para explorar o grafo.
+        visited = [False] * self.n
+        self.dfs(0, visited)
+
+        return all(visited)
+
+    def dfs(self, vert, visited):
+        # Essa função é uma implementação da busca em profundidade (DFS).
+        # Ela é usada para explorar o grafo a partir de um vértice específico.
+        visited[vert] = True
+        # Marca o vértice atual como visitado, definindo seu estado como 'True' na lista 'visited'.
+        for neighbor in self.get_neighbors(vert):
+            # Altera sobre os vizinhos do vértice 'vert'.
+            if not visited[neighbor]:
+                # Se o vizinho não foi visitado (estado 'False' na lista 'visited'), realiza a DFS recursivamente nele.
+                self.dfs(neighbor, visited)
+
+    def is_strongly_connected(self):
+        # Esta função verifica se o grafo é fortemente conectado (para grafos direcionados).
+        # Ela utiliza duas etapas de busca em profundidade (DFS) para explorar o grafo e verificar a conectividade.
+        for vert in range(self.n):
+            # Inicializa uma lista 'visited' com 'False' para todos os vértices.
+            visited = [False] * self.n
+            # Realiza uma busca em profundidade (DFS) a partir do vértice atual.
+            self.dfs(vert, visited)
+            if not all(visited):
+                # Se, após a DFS, algum vértice não foi visitado, o grafo não é fortemente conectado.
+                # Inverta o grafo (troque as direções das arestas)
+                reversed_test = [[self.graph[j][i]
+                # Cria uma versão invertida do grafo, trocando as direções das arestas.
+                                   for j in range(self.n)] for i in range(self.n)]
+
+        reversed_graph = Matrix(self.n, self.digraph)
+        reversed_graph.graph = reversed_test
+        for vert in range(self.n):
+            visited = [False] * self.n
+            # Inicializa uma lista 'visited' com 'False' para todos os vértices.
+            reversed_graph.dfs(vert, visited)
+            # Realiza uma busca em profundidade (DFS) no grafo invertido.
+            if not all(visited):
+                # Se, após a segunda DFS, algum vértice não foi visitado, o grafo não é fortemente conectado.
+                return False
+
+        return True
 
     def __str__(self):
         graph_str = ""
